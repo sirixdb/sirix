e8124d562c8bbc5c5f56c2766ad0a7bf486f0ed3
Johannes Lichtenberger <johannes.lichtenberger@uni-konstanz.de>
2012-10-01 20:39:23 +0200

    [ADD] Missing classes.

---
+++ bundles/sirix-core/src/main/java/org/sirix/api/visitor/IVisitResult.java
@@ -0,0 +1,11 @@
+package org.sirix.api.visitor;
+
+/**
+ * Marker interface. For the {@link VisitorDescendantAxis} use the return types
+ * specified in {@link EVisitResult} to guide the traversal.
+ * 
+ * @author Johannes Lichtenberger
+ *
+ */
+public interface IVisitResult {
+}
