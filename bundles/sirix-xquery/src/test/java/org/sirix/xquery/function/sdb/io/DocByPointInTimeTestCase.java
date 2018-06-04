896ad78f407966b8633eb19abe01ee5f071090a3
Johannes Lichtenberger <lichtenberger.johannes@gmail.com>
2018-06-04 13:47:50 +0200

    Fixed package name.

--- bundles/sirix-xquery/src/test/java/org/sirix/xquery/function/sdb/io/DocByPointInTimeTestCase.java
+++ bundles/sirix-xquery/src/test/java/org/sirix/xquery/function/sdb/io/DocByPointInTimeTestCase.java
@@ -25,7 +25,7 @@
  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
-package function.sdb.io;
+package org.sirix.xquery.function.sdb.io;
 
 import java.io.IOException;
 import java.nio.file.Path;
