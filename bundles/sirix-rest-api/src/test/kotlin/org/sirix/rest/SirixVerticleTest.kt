732a206dc79985cafca5b34318e940a0c3e76bfb
Johannes Lichtenberger <lichtenberger.johannes@gmail.com>
2018-12-14 23:46:34 +0100

    Updated test, still not working as maybe the automatic Vert.x WebClient(Session) redirects following is not working.

--- bundles/sirix-rest-api/src/test/kotlin/org/sirix/rest/SirixVerticleTest.kt
+++ bundles/sirix-rest-api/src/test/kotlin/org/sirix/rest/SirixVerticleTest.kt
@@ -10,6 +10,7 @@ import io.vertx.ext.unit.TestContext
 import io.vertx.ext.unit.junit.VertxUnitRunner
 import io.vertx.ext.web.client.WebClient
 import io.vertx.ext.web.client.WebClientOptions
+import io.vertx.ext.web.client.WebClientSession
 import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -29,28 +30,23 @@ class SirixVerticleTest {
     @Test
     fun testPut(context: TestContext) {
         val resource: Buffer = Buffer.buffer("<xml>foo<bar/></xml>")
-        val client = WebClient.create(vertx, WebClientOptions().setTrustAll(true))
+        val client = WebClient.create(vertx, WebClientOptions().setTrustAll(true).setFollowRedirects(true))
+        val sessionClient = WebClientSession.create(client)
 
         val async = context.async()
 
-        client.putAbs("https://localhost:10443/database/resource1").ssl(true).sendBuffer(resource) { ar ->
-            if (ar.succeeded() && 302 == ar.result().statusCode()) {
+        sessionClient.putAbs("https://localhost:10443/database/resource1").ssl(true).followRedirects(true).sendBuffer(resource) { ar ->
+            if (ar.succeeded() && 200 == ar.result().statusCode()) {
                 val form = MultiMap.caseInsensitiveMultiMap()
                 form.set("username", "admin")
                 form.set("password", "admin")
-                form.set("client_id", "sirix")
-                form.set("client_secret", "c8b9b4ed-67bb-47d9-bd73-a3babc470b2c")
-                form.set("grant_type", "authorization_code")
 
-                client.postAbs("http://localhost:8080/auth/realms/master/protocol/openid-connect/auth?client_id=sirix&client_secret=c8b9b4ed-67bb-47d9-bd73-a3babc470b2c&grant_type=authorization_code")
-                        .putHeader(HttpHeaders.REFERER.toString(), "http://localhost:8080/auth/realms/master/protocol/openid-connect/auth?state=%2Fdatabase%2Fresource1&response_type=code&client_id=sirix")
+                sessionClient.postAbs("http://localhost:8080/auth/realms/master/login-actions/authenticate?client_id=sirix")
                         .putHeader(HttpHeaders.CONTENT_TYPE.toString(), "application/x-www-form-urlencoded")
+                        .putHeader(HttpHeaders.REFERER.toString(), "http://localhost:8080/auth/realms/master/protocol/openid-connect/auth?state=%2Fdatabase%2Fresource1&response_type=code&client_id=sirix")
                         .putHeader(HttpHeaders.CONTENT_LENGTH.toString(), form.toString().length.toString())
                         .sendForm(form) { response ->
                             if (response.succeeded() && 200 == response.result().statusCode()) {
-                                val authJson = response.result().bodyAsJsonObject()
-                                val accessToken = authJson.getString("access_token")
-                                val header = "Bearer $accessToken"
                                 println("okay")
                             } else {
                                 println("not okay")
@@ -60,6 +56,6 @@ class SirixVerticleTest {
             }
         }
 
-        async.awaitSuccess(500000)
+        async.awaitSuccess(5000)
     }
 }
