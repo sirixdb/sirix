a8570f2c84b58a39f3bf56bb96c7392bf2a6aaa0
Johannes Lichtenberger <lichtenberger.johannes@gmail.com>
2018-12-15 13:12:59 +0100

    Updated test case.
    Docker-container is still missing.

--- bundles/sirix-rest-api/src/test/kotlin/org/sirix/rest/SirixVerticleTest.kt
+++ bundles/sirix-rest-api/src/test/kotlin/org/sirix/rest/SirixVerticleTest.kt
@@ -11,9 +11,12 @@ import io.vertx.ext.unit.junit.VertxUnitRunner
 import io.vertx.ext.web.client.WebClient
 import io.vertx.ext.web.client.WebClientOptions
 import io.vertx.ext.web.client.WebClientSession
+import org.jsoup.Jsoup
+import org.junit.After
 import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
+import kotlin.test.assertEquals
 
 
 @RunWith(VertxUnitRunner::class)
@@ -23,38 +26,151 @@ class SirixVerticleTest {
     @Before
     fun setup(context: TestContext) {
         vertx = Vertx.vertx()
-        val options = DeploymentOptions().setConfig(JsonObject().put("https.port", 10443))
+        val options = DeploymentOptions().setConfig(JsonObject().put("https.port", 9443))
         vertx.deployVerticle("org.sirix.rest.SirixVerticle", options, context.asyncAssertSuccess())
+    }
+
+    @After
+    fun tearDown(context: TestContext) {
+        vertx.close(context.asyncAssertSuccess())
     }
 
     @Test
     fun testPut(context: TestContext) {
-        val resource: Buffer = Buffer.buffer("<xml>foo<bar/></xml>")
-        val client = WebClient.create(vertx, WebClientOptions().setTrustAll(true).setFollowRedirects(true))
+        val client = WebClient.create(vertx, WebClientOptions().setTrustAll(true).setFollowRedirects(false))
         val sessionClient = WebClientSession.create(client)
 
         val async = context.async()
 
-        sessionClient.putAbs("https://localhost:10443/database/resource1").ssl(true).followRedirects(true).sendBuffer(resource) { ar ->
-            if (ar.succeeded() && 200 == ar.result().statusCode()) {
-                val form = MultiMap.caseInsensitiveMultiMap()
-                form.set("username", "admin")
-                form.set("password", "admin")
+        val resource = Buffer.buffer("<xml>foo<bar/></xml>")
+        val server = "https://localhost:9443"
+        val serverPath = "/database/resource1"
 
-                sessionClient.postAbs("http://localhost:8080/auth/realms/master/login-actions/authenticate?client_id=sirix")
-                        .putHeader(HttpHeaders.CONTENT_TYPE.toString(), "application/x-www-form-urlencoded")
-                        .putHeader(HttpHeaders.REFERER.toString(), "http://localhost:8080/auth/realms/master/protocol/openid-connect/auth?state=%2Fdatabase%2Fresource1&response_type=code&client_id=sirix")
-                        .putHeader(HttpHeaders.CONTENT_LENGTH.toString(), form.toString().length.toString())
-                        .sendForm(form) { response ->
+        val expectString = """
+        <rest:sequence xmlns:rest="https://sirix.io/rest">
+          <rest:item>
+            <xml rest:id="1">
+              foo
+              <bar rest:id="3"/>
+            </xml>
+          </rest:item>
+        </rest:sequence>
+        """.trimIndent()
+
+        sessionClient.putAbs("$server$serverPath")
+                .ssl(true)
+                .followRedirects(false)
+                .sendBuffer(resource) { response ->
+                    if (response.succeeded() && 302 == response.result().statusCode()) {
+                        val location = response.result().getHeader(HttpHeaders.LOCATION.toString())
+
+                        sessionClient.getAbs(location).send() { response ->
                             if (response.succeeded() && 200 == response.result().statusCode()) {
-                                println("okay")
-                            } else {
-                                println("not okay")
+                                val document = Jsoup.parse(response.result().bodyAsString())
+
+                                val formElement = document.getElementById("kc-form-login")
+                                val url = formElement.attr("action")
+
+                                val form = MultiMap.caseInsensitiveMultiMap()
+                                form.set("username", "admin")
+                                form.set("password", "admin")
+
+                                sessionClient.postAbs(url)
+                                        .putHeader(HttpHeaders.CONTENT_TYPE.toString(), "application/x-www-form-urlencoded")
+                                        .putHeader(HttpHeaders.CONTENT_LENGTH.toString(), form.toString().length.toString())
+                                        .sendForm(form) { response ->
+                                            if (response.succeeded() && 302 == response.result().statusCode()) {
+                                                val location = response.result().getHeader(HttpHeaders.LOCATION.toString())
+
+                                                sessionClient.getAbs(location).ssl(true).send() { response ->
+                                                    if (response.succeeded() && 302 == response.result().statusCode()) {
+                                                        val location = response.result().getHeader(HttpHeaders.LOCATION.toString())
+
+                                                        sessionClient.putAbs("$server$location").sendBuffer(resource) { response ->
+                                                            if (response.succeeded() && 200 == response.result().statusCode()) {
+                                                                assertEquals(expectString.replace("\n", System.getProperty("line.separator")), response.result().bodyAsString().replace("\r\n", System.getProperty("line.separator")))
+
+                                                                async.complete()
+                                                            }
+                                                        }
+                                                    }
+                                                }
+                                            }
+                                        }
                             }
-                            async.complete()
                         }
-            }
-        }
+                    }
+                }
+
+        async.awaitSuccess(500000)
+    }
+
+    @Test
+    fun testGet(context: TestContext) {
+        val client = WebClient.create(vertx, WebClientOptions().setTrustAll(true).setFollowRedirects(false))
+        val sessionClient = WebClientSession.create(client)
+
+        val server = "https://localhost:9443"
+        val serverPath = "/database/resource1"
+
+        val expectString = """
+        <rest:sequence xmlns:rest="https://sirix.io/rest">
+          <rest:item>
+            <xml rest:id="1">
+              foo
+              <bar rest:id="3"/>
+            </xml>
+          </rest:item>
+        </rest:sequence>
+        """.trimIndent()
+
+        val async = context.async()
+
+        sessionClient.getAbs("$server$serverPath")
+                .ssl(true)
+                .followRedirects(false)
+                .send() { response ->
+                    if (response.succeeded() && 302 == response.result().statusCode()) {
+                        val location = response.result().getHeader(HttpHeaders.LOCATION.toString())
+
+                        sessionClient.getAbs(location).send() { response ->
+                            if (response.succeeded() && 200 == response.result().statusCode()) {
+                                val document = Jsoup.parse(response.result().bodyAsString())
+
+                                val formElement = document.getElementById("kc-form-login")
+                                val url = formElement.attr("action")
+
+                                val form = MultiMap.caseInsensitiveMultiMap()
+                                form.set("username", "admin")
+                                form.set("password", "admin")
+
+                                sessionClient.postAbs(url)
+                                        .putHeader(HttpHeaders.CONTENT_TYPE.toString(), "application/x-www-form-urlencoded")
+                                        .putHeader(HttpHeaders.CONTENT_LENGTH.toString(), form.toString().length.toString())
+                                        .sendForm(form) { response ->
+                                            if (response.succeeded() && 302 == response.result().statusCode()) {
+                                                val location = response.result().getHeader(HttpHeaders.LOCATION.toString())
+
+                                                sessionClient.getAbs(location).ssl(true).send() { response ->
+                                                    if (response.succeeded() && 302 == response.result().statusCode()) {
+                                                        val location = response.result().getHeader(HttpHeaders.LOCATION.toString())
+
+                                                        sessionClient.getAbs("$server$location").send() { response ->
+                                                            if (response.succeeded() && 200 == response.result().statusCode()) {
+                                                                assertEquals(expectString.replace("\n", System.getProperty("line.separator")),
+                                                                        response.result().bodyAsString().replace("\r\n", System.getProperty("line.separator")))
+
+                                                                async.complete()
+                                                            }
+                                                        }
+                                                    }
+                                                }
+                                            }
+                                        }
+                            }
+                        }
+                    }
+                }
 
         async.awaitSuccess(5000)
     }
