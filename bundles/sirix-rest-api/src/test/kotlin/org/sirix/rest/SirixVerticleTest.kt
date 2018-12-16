5646bb62e6f38494f3243f962c83783ed844fd1c
Johannes Lichtenberger <lichtenberger.johannes@gmail.com>
2018-12-16 22:29:57 +0100

    Updated tests to use coroutines.

--- bundles/sirix-rest-api/src/test/kotlin/org/sirix/rest/SirixVerticleTest.kt
+++ bundles/sirix-rest-api/src/test/kotlin/org/sirix/rest/SirixVerticleTest.kt
@@ -6,28 +6,42 @@ import io.vertx.core.Vertx
 import io.vertx.core.buffer.Buffer
 import io.vertx.core.http.HttpHeaders
 import io.vertx.core.json.JsonObject
+import io.vertx.ext.unit.Async
 import io.vertx.ext.unit.TestContext
 import io.vertx.ext.unit.junit.VertxUnitRunner
+import io.vertx.ext.web.client.HttpResponse
 import io.vertx.ext.web.client.WebClient
 import io.vertx.ext.web.client.WebClientOptions
 import io.vertx.ext.web.client.WebClientSession
+import io.vertx.kotlin.coroutines.dispatcher
+import io.vertx.kotlin.ext.web.client.sendAwait
+import io.vertx.kotlin.ext.web.client.sendBufferAwait
+import io.vertx.kotlin.ext.web.client.sendFormAwait
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 import org.jsoup.Jsoup
 import org.junit.After
 import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
-import kotlin.test.assertEquals
-
 
 @RunWith(VertxUnitRunner::class)
 class SirixVerticleTest {
-    lateinit var vertx: Vertx
+    private lateinit var vertx: Vertx
+    private lateinit var client: WebClient
+    private lateinit var sessionClient: WebClientSession
+
+    private val server = "https://localhost:9443"
+    private val serverPath = "/database/resource1"
 
     @Before
     fun setup(context: TestContext) {
         vertx = Vertx.vertx()
         val options = DeploymentOptions().setConfig(JsonObject().put("https.port", 9443))
         vertx.deployVerticle("org.sirix.rest.SirixVerticle", options, context.asyncAssertSuccess())
+
+        client = WebClient.create(vertx, WebClientOptions().setTrustAll(true).setFollowRedirects(false))
+        sessionClient = WebClientSession.create(client)
     }
 
     @After
@@ -37,141 +51,139 @@ class SirixVerticleTest {
 
     @Test
     fun testPut(context: TestContext) {
-        val client = WebClient.create(vertx, WebClientOptions().setTrustAll(true).setFollowRedirects(false))
-        val sessionClient = WebClientSession.create(client)
-
         val async = context.async()
-
-        val resource = Buffer.buffer("<xml>foo<bar/></xml>")
-        val server = "https://localhost:9443"
-        val serverPath = "/database/resource1"
-
-        val expectString = """
-        <rest:sequence xmlns:rest="https://sirix.io/rest">
-          <rest:item>
-            <xml rest:id="1">
-              foo
-              <bar rest:id="3"/>
-            </xml>
-          </rest:item>
-        </rest:sequence>
-        """.trimIndent()
-
-        sessionClient.putAbs("$server$serverPath")
-                .ssl(true)
-                .followRedirects(false)
-                .sendBuffer(resource) { response ->
-                    if (response.succeeded() && 302 == response.result().statusCode()) {
-                        val location = response.result().getHeader(HttpHeaders.LOCATION.toString())
-
-                        sessionClient.getAbs(location).send() { response ->
-                            if (response.succeeded() && 200 == response.result().statusCode()) {
-                                val document = Jsoup.parse(response.result().bodyAsString())
-
-                                val formElement = document.getElementById("kc-form-login")
-                                val url = formElement.attr("action")
-
-                                val form = MultiMap.caseInsensitiveMultiMap()
-                                form.set("username", "admin")
-                                form.set("password", "admin")
-
-                                sessionClient.postAbs(url)
-                                        .putHeader(HttpHeaders.CONTENT_TYPE.toString(), "application/x-www-form-urlencoded")
-                                        .putHeader(HttpHeaders.CONTENT_LENGTH.toString(), form.toString().length.toString())
-                                        .sendForm(form) { response ->
-                                            if (response.succeeded() && 302 == response.result().statusCode()) {
-                                                val location = response.result().getHeader(HttpHeaders.LOCATION.toString())
-
-                                                sessionClient.getAbs(location).ssl(true).send() { response ->
-                                                    if (response.succeeded() && 302 == response.result().statusCode()) {
-                                                        val location = response.result().getHeader(HttpHeaders.LOCATION.toString())
-
-                                                        sessionClient.putAbs("$server$location").sendBuffer(resource) { response ->
-                                                            if (response.succeeded() && 200 == response.result().statusCode()) {
-                                                                assertEquals(expectString.replace("\n", System.getProperty("line.separator")), response.result().bodyAsString().replace("\r\n", System.getProperty("line.separator")))
-
-                                                                async.complete()
-                                                            }
-                                                        }
-                                                    }
-                                                }
-                                            }
-                                        }
-                            }
-                        }
-                    }
-                }
-
-        async.awaitSuccess(500000)
-    }
-
-    @Test
-    fun testGet(context: TestContext) {
-        val client = WebClient.create(vertx, WebClientOptions().setTrustAll(true).setFollowRedirects(false))
-        val sessionClient = WebClientSession.create(client)
-
-        val server = "https://localhost:9443"
-        val serverPath = "/database/resource1"
-
-        val expectString = """
-        <rest:sequence xmlns:rest="https://sirix.io/rest">
-          <rest:item>
-            <xml rest:id="1">
-              foo
-              <bar rest:id="3"/>
-            </xml>
-          </rest:item>
-        </rest:sequence>
-        """.trimIndent()
-
-        val async = context.async()
-
-        sessionClient.getAbs("$server$serverPath")
-                .ssl(true)
-                .followRedirects(false)
-                .send() { response ->
-                    if (response.succeeded() && 302 == response.result().statusCode()) {
-                        val location = response.result().getHeader(HttpHeaders.LOCATION.toString())
-
-                        sessionClient.getAbs(location).send() { response ->
-                            if (response.succeeded() && 200 == response.result().statusCode()) {
-                                val document = Jsoup.parse(response.result().bodyAsString())
-
-                                val formElement = document.getElementById("kc-form-login")
-                                val url = formElement.attr("action")
-
-                                val form = MultiMap.caseInsensitiveMultiMap()
-                                form.set("username", "admin")
-                                form.set("password", "admin")
-
-                                sessionClient.postAbs(url)
-                                        .putHeader(HttpHeaders.CONTENT_TYPE.toString(), "application/x-www-form-urlencoded")
-                                        .putHeader(HttpHeaders.CONTENT_LENGTH.toString(), form.toString().length.toString())
-                                        .sendForm(form) { response ->
-                                            if (response.succeeded() && 302 == response.result().statusCode()) {
-                                                val location = response.result().getHeader(HttpHeaders.LOCATION.toString())
-
-                                                sessionClient.getAbs(location).ssl(true).send() { response ->
-                                                    if (response.succeeded() && 302 == response.result().statusCode()) {
-                                                        val location = response.result().getHeader(HttpHeaders.LOCATION.toString())
-
-                                                        sessionClient.getAbs("$server$location").send() { response ->
-                                                            if (response.succeeded() && 200 == response.result().statusCode()) {
-                                                                assertEquals(expectString.replace("\n", System.getProperty("line.separator")),
-                                                                        response.result().bodyAsString().replace("\r\n", System.getProperty("line.separator")))
-
-                                                                async.complete()
-                                                            }
-                                                        }
-                                                    }
-                                                }
-                                            }
-                                        }
-                            }
-                        }
-                    }
-                }
+        context.verify {
+            GlobalScope.launch(vertx.dispatcher()) {
+                executePut(context, async)
+            }
+        }
 
         async.awaitSuccess(5000)
+    }
+
+    private suspend fun SirixVerticleTest.executePut(context: TestContext, async: Async) {
+        val expectString = """
+                    <rest:sequence xmlns:rest="https://sirix.io/rest">
+                      <rest:item>
+                        <xml rest:id="1">
+                          foo
+                          <bar rest:id="3"/>
+                        </xml>
+                      </rest:item>
+                    </rest:sequence>
+                    """.trimIndent()
+
+        val resource = Buffer.buffer("<xml>foo<bar/></xml>")
+
+        var response = sessionClient.putAbs("$server$serverPath")
+                .ssl(true)
+                .followRedirects(false)
+                .sendBufferAwait(resource)
+
+        if (302 == response.statusCode()) {
+            val location = response.getHeader(HttpHeaders.LOCATION.toString())
+
+            response = sessionClient.getAbs(location).sendAwait()
+
+            if (200 == response.statusCode()) {
+                response = sendFormForLoginPage(response, sessionClient)
+
+                if (302 == response.statusCode()) {
+                    val location = response.getHeader(HttpHeaders.LOCATION.toString())
+
+                    response = sessionClient.getAbs(location).ssl(true).sendAwait()
+
+                    if (302 == response.statusCode()) {
+                        val location = response.getHeader(HttpHeaders.LOCATION.toString())
+
+                        response = sessionClient.putAbs("$server$location").sendBufferAwait(resource)
+
+                        if (200 == response.statusCode()) {
+                            context.assertEquals(expectString.replace("\n", System.getProperty("line.separator")),
+                                    response.bodyAsString().replace("\r\n", System.getProperty("line.separator")))
+
+                            async.complete()
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    private suspend fun sendFormForLoginPage(response: HttpResponse<Buffer>, sessionClient: WebClientSession): HttpResponse<Buffer> {
+        var response1 = response
+        val document = Jsoup.parse(response1.bodyAsString())
+
+        val formElement = document.getElementById("kc-form-login")
+        val url = formElement.attr("action")
+
+        val form = MultiMap.caseInsensitiveMultiMap()
+        form.set("username", "admin")
+        form.set("password", "admin")
+
+        response1 = sessionClient.postAbs(url)
+                .putHeader(HttpHeaders.CONTENT_TYPE.toString(), "application/x-www-form-urlencoded")
+                .putHeader(HttpHeaders.CONTENT_LENGTH.toString(), form.toString().length.toString())
+                .sendFormAwait(form)
+        return response1
+    }
+
+    @Test()
+    fun testGet(context: TestContext) {
+        val async = context.async()
+        context.verify {
+            GlobalScope.launch(vertx.dispatcher()) {
+                val asyncPut = context.async()
+                executePut(context, asyncPut)
+                asyncPut.awaitSuccess(5000)
+
+                val expectString = """
+                <rest:sequence xmlns:rest="https://sirix.io/rest">
+                  <rest:item>
+                    <xml rest:id="1">
+                      foo
+                      <bar rest:id="3"/>
+                    </xml>
+                  </rest:item>
+                </rest:sequence>
+                """.trimIndent()
+
+                var response = sessionClient.getAbs("$server$serverPath")
+                        .ssl(true)
+                        .followRedirects(false)
+                        .sendAwait()
+
+                if (302 == response.statusCode()) {
+                    val location = response.getHeader(HttpHeaders.LOCATION.toString())
+
+                    response = sessionClient.getAbs(location).sendAwait()
+
+                    if (200 == response.statusCode()) {
+                        response = sendFormForLoginPage(response, sessionClient)
+
+                        if (302 == response.statusCode()) {
+                            val location = response.getHeader(HttpHeaders.LOCATION.toString())
+
+                            response = sessionClient.getAbs(location).ssl(true).sendAwait()
+
+                            if (302 == response.statusCode()) {
+                                val location = response.getHeader(HttpHeaders.LOCATION.toString())
+
+                                response = sessionClient.getAbs("$server$location").sendAwait()
+
+                                if (200 == response.statusCode()) {
+                                    context.assertEquals(expectString.replace("\n", System.getProperty("line.separator")),
+                                            response.bodyAsString().replace("\r\n", System.getProperty("line.separator")))
+
+                                    async.complete()
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        async.awaitSuccess(20000)
     }
 }
