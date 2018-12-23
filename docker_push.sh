e09a09472df8b3d469e16c4bdb42fa00acbd48e6
Johannes Lichtenberger <johannes.lichtenberger@uni-konstanz.de>
2018-12-23 19:36:18 +0100

    Update docker_push.sh

--- docker_push.sh
+++ docker_push.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
+cd bundles/sirix-rest-api
+docker build -t sirix-rest-api -f src/main/docker/Dockerfile
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 docker push sirixdb/sirix
