0c2feb833225c0d0914ad4bfbb11f75fe3a14fae
Johannes Lichtenberger <lichtenberger.johannes@gmail.com>
2019-01-12 11:12:52 +0100

    Reads from config file...

--- bundles/sirix-rest-api/src/main/kotlin/org/sirix/rest/Main.kt
+++ bundles/sirix-rest-api/src/main/kotlin/org/sirix/rest/Main.kt
@@ -1,16 +1,8 @@
 package org.sirix.rest
 
-import io.vertx.core.Vertx
-import io.vertx.kotlin.core.deployVerticleAwait
+import io.vertx.core.Launcher
 
-suspend fun main() {
-    val vertx = Vertx.vertx()
-    try {
-        vertx.deployVerticleAwait("org.sirix.rest.SirixVerticle")
-        println("Application started.")
-    } catch (exception: Throwable) {
-        println("Could not start application.")
-        exception.printStackTrace()
-    }
+fun main(args: Array<String>) {
+    Launcher.executeCommand("run", "-conf", "src/main/resources/sirix-conf.json", "org.sirix.rest.SirixVerticle")
 }
 
