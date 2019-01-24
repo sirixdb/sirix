930eb67cc7bf0bfe2790a369245b2593e6084d1e
Johannes Lichtenberger <lichtenberger.johannes@gmail.com>
2019-01-24 18:03:34 +0100

    Refactors name index stuff.

--- bundles/sirix-core/src/main/java/org/sirix/index/name/json/JsonNameIndexListener.java
+++ bundles/sirix-core/src/main/java/org/sirix/index/name/json/JsonNameIndexListener.java
@@ -3,23 +3,17 @@ package org.sirix.index.name.json;
 import javax.annotation.Nonnull;
 import org.brackit.xquery.atomic.QNm;
 import org.sirix.access.trx.node.IndexController.ChangeType;
-import org.sirix.api.PageWriteTrx;
 import org.sirix.index.ChangeListener;
-import org.sirix.index.IndexDef;
 import org.sirix.index.name.NameIndexListener;
-import org.sirix.index.name.NameIndexListenerFactory;
-import org.sirix.node.interfaces.Record;
 import org.sirix.node.interfaces.immutable.ImmutableNode;
 import org.sirix.node.json.ObjectKeyNode;
-import org.sirix.page.UnorderedKeyValuePage;
 
 final class JsonNameIndexListener implements ChangeListener {
 
   private final NameIndexListener mIndexListener;
 
-  public JsonNameIndexListener(final PageWriteTrx<Long, Record, UnorderedKeyValuePage> pageWriteTrx,
-      final IndexDef indexDefinition, final NameIndexListenerFactory listenerFactory) {
-    mIndexListener = listenerFactory.create(pageWriteTrx, indexDefinition);
+  public JsonNameIndexListener(final NameIndexListener listener) {
+    mIndexListener = listener;
   }
 
   @Override
