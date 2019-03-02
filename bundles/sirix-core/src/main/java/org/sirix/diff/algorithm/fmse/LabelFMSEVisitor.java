35b39e3cc03e5938dfa7d965931833d1fc04eccc
Johannes Lichtenberger <lichtenberger.johannes@gmail.com>
2019-03-02 14:07:07 +0100

    Fixes namespace insertion bug

--- bundles/sirix-core/src/main/java/org/sirix/diff/algorithm/fmse/LabelFMSEVisitor.java
+++ bundles/sirix-core/src/main/java/org/sirix/diff/algorithm/fmse/LabelFMSEVisitor.java
@@ -1,6 +1,6 @@
 /**
  * Copyright (c) 2011, University of Konstanz, Distributed Systems Group All rights reserved.
- * 
+ *
  * Redistribution and use in source and binary forms, with or without modification, are permitted
  * provided that the following conditions are met: * Redistributions of source code must retain the
  * above copyright notice, this list of conditions and the following disclaimer. * Redistributions
@@ -8,7 +8,7 @@
  * following disclaimer in the documentation and/or other materials provided with the distribution.
  * * Neither the name of the University of Konstanz nor the names of its contributors may be used to
  * endorse or promote products derived from this software without specific prior written permission.
- * 
+ *
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR
  * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
  * FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL <COPYRIGHT HOLDER> BE LIABLE
@@ -26,19 +26,17 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import org.sirix.access.trx.node.xdm.AbstractXdmNodeVisitor;
-import org.sirix.api.ResourceManager;
 import org.sirix.api.visitor.VisitResultType;
 import org.sirix.api.xdm.XdmNodeReadOnlyTrx;
-import org.sirix.exception.SirixException;
 import org.sirix.node.Kind;
 import org.sirix.node.immutable.xdm.ImmutableElement;
 import org.sirix.node.immutable.xdm.ImmutableText;
 
 /**
  * Label visitor. Treats empty-elements as internal nodes.
- * 
+ *
  * @author Johannes Lichtenberger, University of Konstanz
- * 
+ *
  */
 public final class LabelFMSEVisitor extends AbstractXdmNodeVisitor {
 
@@ -53,19 +51,18 @@ public final class LabelFMSEVisitor extends AbstractXdmNodeVisitor {
 
   /**
    * Constructor.
-   * 
-   * @param pSession {@link ResourceManager} implementation
-   * @throws SirixException if setting up sirix fails
+   *
+   * @param readTrx a read only transaction
    */
-  public LabelFMSEVisitor(final XdmNodeReadOnlyTrx pReadTrx) throws SirixException {
-    mRtx = checkNotNull(pReadTrx);
+  public LabelFMSEVisitor(final XdmNodeReadOnlyTrx readTrx) {
+    mRtx = checkNotNull(readTrx);
     mLabels = new HashMap<>();
     mLeafLabels = new HashMap<>();
   }
 
   @Override
-  public VisitResultType visit(final ImmutableElement pNode) {
-    final long nodeKey = pNode.getNodeKey();
+  public VisitResultType visit(final ImmutableElement node) {
+    final long nodeKey = node.getNodeKey();
     mRtx.moveTo(nodeKey);
     for (int i = 0, nspCount = mRtx.getNamespaceCount(); i < nspCount; i++) {
       mRtx.moveToNamespace(i);
@@ -77,16 +74,16 @@ public final class LabelFMSEVisitor extends AbstractXdmNodeVisitor {
       addLeafLabel();
       mRtx.moveTo(nodeKey);
     }
-    if (!mLabels.containsKey(pNode.getKind())) {
-      mLabels.put(pNode.getKind(), new ArrayList<Long>());
+    if (!mLabels.containsKey(node.getKind())) {
+      mLabels.put(node.getKind(), new ArrayList<Long>());
     }
-    mLabels.get(pNode.getKind()).add(pNode.getNodeKey());
+    mLabels.get(node.getKind()).add(node.getNodeKey());
     return VisitResultType.CONTINUE;
   }
 
   @Override
-  public VisitResultType visit(final ImmutableText pNode) {
-    mRtx.moveTo(pNode.getNodeKey());
+  public VisitResultType visit(final ImmutableText node) {
+    mRtx.moveTo(node.getNodeKey());
     addLeafLabel();
     return VisitResultType.CONTINUE;
   }
@@ -97,14 +94,14 @@ public final class LabelFMSEVisitor extends AbstractXdmNodeVisitor {
   private void addLeafLabel() {
     final Kind nodeKind = mRtx.getKind();
     if (!mLeafLabels.containsKey(nodeKind)) {
-      mLeafLabels.put(nodeKind, new ArrayList<Long>());
+      mLeafLabels.put(nodeKind, new ArrayList<>());
     }
     mLeafLabels.get(nodeKind).add(mRtx.getNodeKey());
   }
 
   /**
    * Get labels.
-   * 
+   *
    * @return the Labels
    */
   public Map<Kind, List<Long>> getLabels() {
@@ -113,7 +110,7 @@ public final class LabelFMSEVisitor extends AbstractXdmNodeVisitor {
 
   /**
    * Get leaf labels.
-   * 
+   *
    * @return the leaf labels
    */
   public Map<Kind, List<Long>> getLeafLabels() {
