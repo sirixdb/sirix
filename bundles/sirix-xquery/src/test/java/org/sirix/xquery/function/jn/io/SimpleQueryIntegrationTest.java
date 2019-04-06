9177222450ed0deb458d19971b608306ae8319e2
Johannes Lichtenberger <lichtenberger.johannes@gmail.com>
2019-04-06 15:59:10 +0200

    Working on time-travel integration test

--- bundles/sirix-xquery/src/test/java/org/sirix/xquery/function/jn/io/SimpleQueryIntegrationTest.java
+++ bundles/sirix-xquery/src/test/java/org/sirix/xquery/function/jn/io/SimpleQueryIntegrationTest.java
@@ -1,14 +1,19 @@
 package org.sirix.xquery.function.jn.io;
 
+import java.io.IOException;
 import java.io.PrintStream;
 import org.brackit.xquery.XQuery;
 import org.brackit.xquery.util.io.IOUtils;
 import org.brackit.xquery.util.serialize.StringSerializer;
 import org.brackit.xquery.xdm.Sequence;
 import org.junit.Test;
+import org.sirix.api.json.JsonNodeTrx;
+import org.sirix.service.json.shredder.JsonShredder;
+import org.sirix.utils.JsonDocumentCreator;
 import org.sirix.xquery.SirixCompileChain;
 import org.sirix.xquery.SirixQueryContext;
 import org.sirix.xquery.json.BasicJsonDBStore;
+import org.sirix.xquery.json.JsonDBObject;
 import junit.framework.TestCase;
 
 public final class SimpleQueryIntegrationTest extends TestCase {
@@ -171,36 +176,41 @@ public final class SimpleQueryIntegrationTest extends TestCase {
     }
   }
 
-  // @Test
-  // public void testTimeTravel() {
-  // // Initialize query context and store.
-  // try (final BasicJsonDBStore store = BasicJsonDBStore.newBuilder().build();
-  // final SirixQueryContext ctx = SirixQueryContext.createWithJsonStore(store);
-  // final SirixCompileChain chain = SirixCompileChain.createWithJsonStore(store)) {
-  //
-  // // Use XQuery to store a JSON string into the store.
-  // System.out.println("Storing document:");
-  // final String storeQuery = "jn:store('mycol.jn','mydoc.jn','" + mJson + "')";
-  // System.out.println(storeQuery);
-  // new XQuery(chain, storeQuery).evaluate(ctx);
-  //
-  // // Use XQuery to load a JSON database/resource.
-  // System.out.println("Opening document again:");
-  // final String openDocQuery = "jn:doc('mycol.jn','mydoc.jn')";
-  // System.out.println(openDocQuery);
-  // final Sequence seq = (JSONDocnew XQuery(chain, openDocQuery).evaluate(ctx);
-  //
-  // // Use XQuery to load a JSON database/resource.
-  // System.out.println("Opening document again:");
-  // final String openQuery = "jn:doc('mycol.jn','mydoc.jn')=>sirix[[0]]=>revisionNumber";
-  // System.out.println(openQuery);
-  // final Sequence openSeq = new XQuery(chain, openQuery).evaluate(ctx);
-  //
-  // assertNotNull(openSeq);
-  //
-  // final PrintStream buf = IOUtils.createBuffer();
-  // new StringSerializer(buf).serialize(openSeq);
-  // assertEquals("1", buf.toString());
-  // }
-  // }
+  @Test
+  public void testTimeTravel() throws IOException {
+    // Initialize query context and store.
+    try (final BasicJsonDBStore store = BasicJsonDBStore.newBuilder().build();
+        final SirixQueryContext ctx = SirixQueryContext.createWithJsonStore(store);
+        final SirixCompileChain chain = SirixCompileChain.createWithJsonStore(store)) {
+
+      final String storeQuery = "jn:store('mycol.jn','mydoc.jn','" + JsonDocumentCreator.JSON + "')";
+      new XQuery(chain, storeQuery).evaluate(ctx);
+
+      final String openDocQuery = "jn:doc('mycol.jn','mydoc.jn')";
+      final JsonDBObject object = (JsonDBObject) new XQuery(chain, openDocQuery).evaluate(ctx);
+
+      try (final JsonNodeTrx wtx = object.getTrx().getResourceManager().beginNodeTrx()) {
+        wtx.moveTo(3);
+
+        try (final var reader = JsonShredder.createStringReader("{\"foo\":\"bar\"}")) {
+          wtx.insertSubtreeAsFirstChild(reader);
+
+          wtx.moveTo(11);
+          wtx.remove();
+          wtx.commit();
+        }
+
+        assert wtx.getRevisionNumber() == 4;
+      }
+
+      // final String openQuery = "jn:doc('mycol.jn','mydoc.jn')=>sirix[[0]]=>revisionNumber";
+      // final Sequence openSeq = new XQuery(chain, openQuery).evaluate(ctx);
+      //
+      // assertNotNull(openSeq);
+      //
+      // final PrintStream buf = IOUtils.createBuffer();
+      // new StringSerializer(buf).serialize(openSeq);
+      // assertEquals("1", buf.toString());
+    }
+  }
 }
