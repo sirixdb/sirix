2e36b6e1d878a4a35a2c5ff1e77ca4fabc5e5c47
b0n541 <j@nschaefer.net>
2019-10-01 19:23:39 +0200

    Add module info for xquery, examples, fs

---
+++ bundles/sirix-fs/src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module sirix.fs {
+    requires brackit;
+    requires com.google.common;
+    requires jsr305;
+    requires slf4j.api;
+
+    requires sirix.core;
+}
---
+++ bundles/sirix-xquery/src/main/java/module-info.java
@@ -0,0 +1,14 @@
+module sirix.xquery {
+    requires brackit;
+    requires com.google.common;
+    requires google.http.client;
+    requires gson;
+    requires java.xml;
+    requires jsr305;
+    requires slf4j.api;
+
+    requires sirix.core;
+
+    exports org.sirix.xquery;
+    exports org.sirix.xquery.node;
+}
