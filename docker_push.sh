786e666095104c676dae40523f8d6e979942b7cf
Jayadeep KM <kmjayadeep@gmail.com>
2019-10-04 12:57:28 +0530

    fixed docker run issue, modified docker push script
    Signed-off-by: Jayadeep KM <kmjayadeep@gmail.com>

--- docker_push.sh
+++ docker_push.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 cd bundles/sirix-rest-api
-mvn clean package -DskipTests
 docker-compose build
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 docker-compose push
