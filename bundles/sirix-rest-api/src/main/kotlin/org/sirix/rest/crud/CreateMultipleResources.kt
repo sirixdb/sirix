195a7a849b7b0a81351a0e6c08560803ce794ff5
Johannes Lichtenberger <lichtenberger.johannes@gmail.com>
2019-10-05 18:56:47 +0200

    Add refactoring and performance tweak

--- bundles/sirix-rest-api/src/main/kotlin/org/sirix/rest/crud/CreateMultipleResources.kt
+++ bundles/sirix-rest-api/src/main/kotlin/org/sirix/rest/crud/CreateMultipleResources.kt
@@ -9,26 +9,32 @@ import java.nio.file.Path
 class CreateMultipleResources(private val location: Path) {
     suspend fun handle(ctx: RoutingContext): Route {
         val fileUploads = ctx.fileUploads()
-        var xmlCount = 0
-        var jsonCount = 0
+        var isXmlFiles = false
+        var isJsonFiles = false
 
         fileUploads.forEach { fileUpload ->
             when (fileUpload.contentType()) {
-                "application/xml" -> xmlCount++
-                "application/json" -> jsonCount++
+                "application/xml" -> {
+                    if (isJsonFiles) {
+                        ctx.fail(IllegalArgumentException("All uploaded files must be either of type XML or JSON."))
+                        return ctx.currentRoute()
+                    }
+
+                    isXmlFiles = true
+                }
+                "application/json" -> {
+                    if (isXmlFiles) {
+                        ctx.fail(IllegalArgumentException("All uploaded files must be either of type XML or JSON."))
+                        return ctx.currentRoute()
+                    }
+
+                    isJsonFiles = true
+                }
             }
         }
 
-        if (xmlCount > 0 && xmlCount != fileUploads.size) {
-            ctx.fail(IllegalArgumentException("All uploaded files must be either of type XML or JSON."))
-        } else if (jsonCount > 0 && jsonCount != fileUploads.size) {
-            ctx.fail(IllegalArgumentException("All uploaded files must be either of type XML or JSON."))
-        }
-
-        if (ctx.failed()) return ctx.currentRoute()
-
-        if (xmlCount > 0) XmlCreate(location, true).handle(ctx)
-        else if (jsonCount > 0) JsonCreate(location, true).handle(ctx)
+        if (isXmlFiles) XmlCreate(location, true).handle(ctx)
+        else if (isJsonFiles) JsonCreate(location, true).handle(ctx)
 
         return ctx.currentRoute()
     }
