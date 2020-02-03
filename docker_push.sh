053750f784f2c6dbe60d83a3442dd3b6335ba6d4
Johannes Lichtenberger <johannes.lichtenberger@uni-konstanz.de>
2020-02-03 18:32:52 +0100

    Update docker_push.sh

--- docker_push.sh
+++ docker_push.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-docker-compose build --no-cache server
+docker-compose stop && docker-compose rm && docker-compose build --no-cache server
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 docker-compose push server
