e682ab426fc718430eca2dde05a897e07a3f164f
Johannes Lichtenberger <johannes.lichtenberger@unitedplanet.de>
2020-02-12 19:21:20 +0100

    Fix client secret

--- bundles/sirix-rest-api/src/test/kotlin/org/sirix/rest/SirixVerticleJsonTest.kt
+++ bundles/sirix-rest-api/src/test/kotlin/org/sirix/rest/SirixVerticleJsonTest.kt
@@ -44,7 +44,7 @@ class SirixVerticleJsonTest {
     fun setup(vertx: Vertx, testContext: VertxTestContext) {
         val options = DeploymentOptions().setConfig(
             JsonObject().put("https.port", 9443)
-                .put("client.secret", "64aaf9b2-9ea1-43cd-bcb6-87d2f430aaa2")//78a294c4-0492-4e44-a35f-7eb9cab0d831")
+                .put("client.secret", "78a294c4-0492-4e44-a35f-7eb9cab0d831") // "64aaf9b2-9ea1-43cd-bcb6-87d2f430aaa2"
                 .put("keycloak.url", "http://localhost:8080/auth/realms/sirixdb")
         )
         vertx.deployVerticle("org.sirix.rest.SirixVerticle", options, testContext.completing())
