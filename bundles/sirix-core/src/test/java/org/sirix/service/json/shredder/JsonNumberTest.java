772c8df4784d091eb006b42d97eeb34d28a1b1f8
Mikael Carlsson <mikkar2@kth.se>
2020-02-12 14:44:31 +0100

    Refactor json number fix #24: Moved code that handles decimal numbers to help function isDecimal. Removed try catch statement that already is covered

--- bundles/sirix-core/src/test/java/org/sirix/service/json/shredder/JsonNumberTest.java
+++ bundles/sirix-core/src/test/java/org/sirix/service/json/shredder/JsonNumberTest.java
@@ -11,55 +11,55 @@ import static org.testng.AssertJUnit.fail;
 
 public final class JsonNumberTest {
 
-    @Test
-    public void testFloat() {
-        Float f = Float.MAX_VALUE -1;
-        String s = Float.toString(f);
-        Number n = JsonNumber.stringToNumber(s);
+  @Test
+  public void testFloat() {
+    Float f = Float.MAX_VALUE - 1;
+    String s = Float.toString(f);
+    Number n = JsonNumber.stringToNumber(s);
 
-        assertTrue("Expected type is Float", n instanceof Float);
+    assertTrue("Expected type is Float", n instanceof Float);
+  }
+
+
+  @Test
+  public void testLong() {
+    Long l = Long.MAX_VALUE - 1;
+    String s = Long.toString(l);
+    Number n = JsonNumber.stringToNumber(s);
+
+    assertTrue("Expected type is Long", n instanceof Long);
+  }
+
+
+  @Test
+  public void testInteger() {
+    Integer i = Integer.MAX_VALUE - 1;
+    String s = Integer.toString(i);
+    Number n = JsonNumber.stringToNumber(s);
+
+    assertTrue("Expected type is Integer", n instanceof Integer);
+  }
+
+
+  @Test
+  public void testBigInteger() {
+    BigInteger b = BigInteger.valueOf(Long.MAX_VALUE).add(BigInteger.ONE);
+    String s = b.toString();
+    Number n = JsonNumber.stringToNumber(s);
+
+    assertTrue("Expected type is BigInteger", n instanceof BigInteger);
+  }
+
+  @Test
+  public void testException() {
+    String s = ("1.0ae10");
+
+    try {
+      JsonNumber.stringToNumber(s);
+      fail("Expected IllegalStateException to be thrown");
+    } catch (IllegalStateException e) {
+      assertTrue(true);
     }
-
-
-    @Test
-    public void testLong(){
-        Long l = Long.MAX_VALUE -1;
-        String s = Long.toString(l);
-        Number n = JsonNumber.stringToNumber(s);
-
-        assertTrue("Expected type is Long", n instanceof Long);
-    }
-
-
-    @Test
-    public void testInteger(){
-        Integer i = Integer.MAX_VALUE -1;
-        String s = Integer.toString(i);
-        Number n = JsonNumber.stringToNumber(s);
-
-        assertTrue("Expected type is Integer", n instanceof Integer);
-    }
-
-
-    @Test
-    public void testBigInteger(){
-        BigInteger b = BigInteger.valueOf(Long.MAX_VALUE).add(BigInteger.ONE);
-        String s = b.toString();
-        Number n = JsonNumber.stringToNumber(s);
-
-        assertTrue("Expected type is BigInteger", n instanceof BigInteger);
-    }
-
-    @Test
-    public void testException() {
-        String s = ("1.0ae10");
-
-        try {
-            JsonNumber.stringToNumber(s);
-            fail("Expected IllegalStateException to be thrown");
-        } catch(IllegalStateException e){
-             assertTrue(true);
-    }
-    }
+  }
 
 }
