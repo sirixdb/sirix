bea5896f4a34979d906f5178ef709fee155220c6
Johannes Lichtenberger <johannes.lichtenberger@unitedplanet.de>
2020-02-22 15:40:36 +0100

    Fix routes

--- bundles/sirix-rest-api/src/main/kotlin/org/sirix/rest/crud/History.kt
+++ bundles/sirix-rest-api/src/main/kotlin/org/sirix/rest/crud/History.kt
@@ -1,93 +1,88 @@
 package org.sirix.rest.crud
 
-import io.netty.handler.codec.http.HttpResponseStatus
 import io.vertx.core.http.HttpHeaders
+import io.vertx.ext.web.Route
 import io.vertx.ext.web.RoutingContext
-import io.vertx.ext.web.handler.impl.HttpStatusException
+import io.vertx.kotlin.coroutines.dispatcher
+import kotlinx.coroutines.withContext
 import org.sirix.access.DatabaseType
-import org.sirix.access.Databases
-import org.sirix.exception.SirixUsageException
+import org.sirix.access.Databases.*
+import org.sirix.api.Database
 import org.sirix.service.json.serialize.StringValue
 import java.nio.charset.StandardCharsets
 import java.nio.file.Path
 
-class History {
-    fun serialize(
-        ctx: RoutingContext,
-        location: Path,
-        databaseName: String,
-        resourceName: String
-    ) {
-        val databaseType = Databases.getDatabaseType(location.resolve(databaseName).toAbsolutePath())
+class History(private val location: Path) {
+    suspend fun handle(ctx: RoutingContext): Route {
+        val databaseName = ctx.pathParam("database")
+        val resourceName = ctx.pathParam("resource")
 
-        @Suppress("WHEN_ENUM_CAN_BE_NULL_IN_JAVA") val database =
-            try {
-                when (databaseType) {
-                    DatabaseType.JSON -> Databases.openJsonDatabase(location.resolve(databaseName))
-                    DatabaseType.XML -> Databases.openXmlDatabase(location.resolve(databaseName))
-                }
-
-            } catch (e: SirixUsageException) {
-                ctx.fail(HttpStatusException(HttpResponseStatus.NOT_FOUND.code(), e))
-                return
+        @Suppress("WHEN_ENUM_CAN_BE_NULL_IN_JAVA") val database: Database<*> =
+            when (getDatabaseType(location.resolve(databaseName).toAbsolutePath())) {
+                DatabaseType.JSON -> openJsonDatabase(location.resolve(databaseName))
+                DatabaseType.XML -> openXmlDatabase(location.resolve(databaseName))
             }
 
-        database.use {
-            val manager = database.openResourceManager(resourceName)
+        withContext(ctx.vertx().dispatcher()) {
+            database.use {
+                val manager = database.openResourceManager(resourceName)
 
-            manager.use {
-                val numberOfRevisions = ctx.queryParam("revisions")
-                val startRevision = ctx.queryParam("startRevision")
-                val endRevision = ctx.queryParam("endRevision")
+                manager.use {
+                    val numberOfRevisions = ctx.queryParam("revisions")
+                    val startRevision = ctx.queryParam("startRevision")
+                    val endRevision = ctx.queryParam("endRevision")
 
-                val buffer = StringBuilder()
+                    val buffer = StringBuilder()
 
-                val historyList = if (numberOfRevisions.isEmpty()) {
-                    if (startRevision.isEmpty() && endRevision.isEmpty()) {
-                        manager.getHistory()
+                    val historyList = if (numberOfRevisions.isEmpty()) {
+                        if (startRevision.isEmpty() && endRevision.isEmpty()) {
+                            manager.getHistory()
+                        } else {
+                            val startRevisionAsInt = startRevision[0].toInt()
+                            val endRevisionAsInt = endRevision[0].toInt()
+                            manager.getHistory(startRevisionAsInt, endRevisionAsInt)
+                        }
                     } else {
-                        val startRevisionAsInt = startRevision[0].toInt()
-                        val endRevisionAsInt = endRevision[0].toInt()
-                        manager.getHistory(startRevisionAsInt, endRevisionAsInt)
+                        val revisions = numberOfRevisions[0].toInt()
+                        manager.getHistory(revisions)
                     }
-                } else {
-                    val revisions = numberOfRevisions[0].toInt()
-                    manager.getHistory(revisions)
-                }
 
-                buffer.append("{\"history\":[")
+                    buffer.append("{\"history\":[")
 
-                historyList.forEachIndexed { index, revisionTuple ->
-                    buffer.append("{\"revision\":")
-                    buffer.append(revisionTuple.revision)
-                    buffer.append(",")
-
-                    buffer.append("\"revisionTimestamp\":\"")
-                    buffer.append(revisionTuple.revisionTimestamp)
-                    buffer.append("\",")
-
-                    buffer.append("\"author\":\"")
-                    buffer.append(StringValue.escape(revisionTuple.user.name))
-                    buffer.append("\",")
-
-                    buffer.append("\"commitMessage\":\"")
-                    buffer.append(StringValue.escape(revisionTuple.commitMessage.orElse("")))
-                    buffer.append("\"}")
-
-                    if (index != historyList.size - 1)
+                    historyList.forEachIndexed { index, revisionTuple ->
+                        buffer.append("{\"revision\":")
+                        buffer.append(revisionTuple.revision)
                         buffer.append(",")
+
+                        buffer.append("\"revisionTimestamp\":\"")
+                        buffer.append(revisionTuple.revisionTimestamp)
+                        buffer.append("\",")
+
+                        buffer.append("\"author\":\"")
+                        buffer.append(StringValue.escape(revisionTuple.user.name))
+                        buffer.append("\",")
+
+                        buffer.append("\"commitMessage\":\"")
+                        buffer.append(StringValue.escape(revisionTuple.commitMessage.orElse("")))
+                        buffer.append("\"}")
+
+                        if (index != historyList.size - 1)
+                            buffer.append(",")
+                    }
+
+                    buffer.append("]}")
+
+                    val content = buffer.toString()
+
+                    ctx.response().setStatusCode(200)
+                        .putHeader(HttpHeaders.CONTENT_TYPE, "application/json")
+                        .putHeader(HttpHeaders.CONTENT_LENGTH, content.toByteArray(StandardCharsets.UTF_8).size.toString())
+                        .write(content)
+                        .end()
                 }
-
-                buffer.append("]}")
-
-                val content = buffer.toString()
-
-                ctx.response().setStatusCode(200)
-                    .putHeader(HttpHeaders.CONTENT_TYPE, "application/json")
-                    .putHeader(HttpHeaders.CONTENT_LENGTH, content.toByteArray(StandardCharsets.UTF_8).size.toString())
-                    .write(content)
-                    .end()
             }
         }
+
+        return ctx.currentRoute()
     }
 }
