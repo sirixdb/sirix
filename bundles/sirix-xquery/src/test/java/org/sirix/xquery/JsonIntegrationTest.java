6f8705939070bf4e9ff8d06529daa3af63906086
Johannes Lichtenberger <johannes.lichtenberger@unitedplanet.de>
2020-05-18 23:44:14 +0200

    Add test cases

--- bundles/sirix-xquery/src/test/java/org/sirix/xquery/JsonIntegrationTest.java
+++ bundles/sirix-xquery/src/test/java/org/sirix/xquery/JsonIntegrationTest.java
@@ -13,9 +13,16 @@ public final class JsonIntegrationTest extends AbstractJsonTest {
   }
 
   @Test
-  public void testNesting() throws IOException {
+  public void testNesting1() throws IOException {
     final String storeQuery = "jn:store('mycol.jn','mydoc.jn','[{\"key\":0},{\"value\":{\"key\":true}},{\"key\":\"hey\",\"value\":false}]')";
     final String openQuery = "for $i in jn:doc('mycol.jn','mydoc.jn')=>value where $i instance of record() and $i=>key eq true() return { $i, \"nodekey\": sdb:nodekey($i) }";
     test(storeQuery, openQuery,"{\"key\":true,\"nodekey\":7}");
+  }
+
+  @Test
+  public void testNesting2() throws IOException {
+    final String storeQuery = "jn:store('mycol.jn','mydoc.jn','[{\"key\":0},{\"value\":[{\"key\":{\"boolean\":true}},{\"newkey\":\"yes\"}]},{\"key\":\"hey\",\"value\":false}]')";
+    final String openQuery = "for $i in jn:doc('mycol.jn','mydoc.jn')=>value=>key return { $i, \"nodekey\": sdb:nodekey($i) }";
+    test(storeQuery, openQuery,"{\"boolean\":true,\"nodekey\":10}");
   }
 }
