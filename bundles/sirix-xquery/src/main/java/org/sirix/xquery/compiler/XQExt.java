dfa63dba5e5192c9fd29a8685220523f3506082e
Johannes Lichtenberger <johannes.lichtenberger@unitedplanet.de>
2020-05-28 18:48:41 +0200

    JsonPathStep optimizations

--- bundles/sirix-xquery/src/main/java/org/sirix/xquery/compiler/XQExt.java
+++ bundles/sirix-xquery/src/main/java/org/sirix/xquery/compiler/XQExt.java
@@ -8,15 +8,15 @@ import org.brackit.xquery.compiler.XQ;
  */
 public final class XQExt {
 
-  private static final int OFFSET = XQ.allocate(1);
+  private static final int OFFSET = XQ.allocate(3);
 
-  public static final int MultiStepExpr = OFFSET + 0;
+  public static final int MultiStepExpr = OFFSET;
 
   public static final int IndexExpr = OFFSET + 1;
 
   public static final int ParentExpr = OFFSET + 2;
 
-  public static final String NAMES[] = new String[] {"MultiStepExpr", "IndexExpr"};
+  public static final String NAMES[] = new String[] {"MultiStepExpr", "IndexExpr", "ParentExpr"};
 
   public static Object toName(int key) {
     return NAMES[key - OFFSET];
--- bundles/sirix-xquery/src/main/java/org/sirix/xquery/compiler/optimizer/walker/JsonPathStep.java
+++ bundles/sirix-xquery/src/main/java/org/sirix/xquery/compiler/optimizer/walker/JsonPathStep.java
@@ -7,17 +7,14 @@ import org.brackit.xquery.compiler.optimizer.walker.Walker;
 import org.brackit.xquery.function.json.JSONFun;
 import org.brackit.xquery.module.StaticContext;
 import org.sirix.access.Databases;
-import org.sirix.axis.IncludeSelf;
-import org.sirix.index.IndexDef;
-import org.sirix.index.IndexType;
-import org.sirix.index.path.summary.PathNode;
-import org.sirix.index.path.summary.PathSummaryReader;
-import org.sirix.node.NodeKind;
 import org.sirix.xquery.compiler.XQExt;
 
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.*;
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.Optional;
 
 public final class JsonPathStep extends Walker {
   /**
@@ -40,82 +37,122 @@ public final class JsonPathStep extends Walker {
       return astNode;
     }
 
-    if (astNode.getChild(0).getType() == XQ.DerefExpr || astNode.getChild(0).getType() == XQ.FunctionCall) {
+    boolean foundDerefAncestor = findDerefAncestor(astNode);
+
+    if (!foundDerefAncestor && (astNode.getChild(0).getType() == XQ.DerefExpr
+        || astNode.getChild(0).getType() == XQ.FunctionCall)) {
       final var pathSegmentNames = new ArrayDeque<String>();
       final var pathSegmentName = astNode.getChild(astNode.getChildCount() - 1).getStringValue();
       pathSegmentNames.add(pathSegmentName);
 
       final var newNode = getPathStep(astNode, pathSegmentNames);
 
-      if (newNode.isPresent()) {
-        final var newChildNode = newNode.get();
+      if (newNode.isEmpty() || pathSegmentNames.size() <= 1) {
+        return astNode;
+      }
 
-        if (new QNm(JSONFun.JSON_NSURI, JSONFun.JSON_PREFIX, "doc").equals(newChildNode.getValue())) {
-          final var databaseName = newChildNode.getChild(0).getStringValue();
-          final var resourceName = newChildNode.getChild(1).getStringValue();
+      final var newChildNode = newNode.get();
 
-          final int revision;
-          if (newChildNode.getChildCount() > 2) {
-            revision = (int) newChildNode.getChild(2).getValue();
-          } else {
-            revision = -1;
+      if (new QNm(JSONFun.JSON_NSURI, JSONFun.JSON_PREFIX, "doc").equals(newChildNode.getValue())) {
+        final var databaseName = newChildNode.getChild(0).getStringValue();
+        final var resourceName = newChildNode.getChild(1).getStringValue();
+
+        final int revision;
+        if (newChildNode.getChildCount() > 2) {
+          revision = (int) newChildNode.getChild(2).getValue();
+        } else {
+          revision = -1;
+        }
+
+        final var path = LOCATION.resolve(databaseName);
+
+        try (final var database = Databases.openJsonDatabase(path);
+             final var resMgr = database.openResourceManager(resourceName);
+             final var pathSummary = revision == -1 ? resMgr.openPathSummary() : resMgr.openPathSummary(revision)) {
+          var pathNodeKeysToRemove = new ArrayList<Integer>();
+          var pathNodeKeys = new ArrayList<Integer>();
+
+          final var pathSegmentNameToCheck = pathSegmentNames.removeFirst();
+
+          var pathNodeKeyBitmap = pathSummary.match(new QNm(pathSegmentNameToCheck), 0);
+
+          if (pathNodeKeyBitmap.isEmpty()) {
+            final var parentASTNode = astNode.getParent();
+            parentASTNode.replaceChild(astNode.getChildIndex(), new AST(XQ.SequenceExpr));
           }
 
-          final var path = LOCATION.resolve(databaseName);
+          for (int i = pathNodeKeyBitmap.nextSetBit(0); i >= 0; i = pathNodeKeyBitmap.nextSetBit(i + 1)) {
+            // operate on index i here
+            pathNodeKeys.add(i);
 
-          try (final var database = Databases.openJsonDatabase(path);
-               final var resMgr = database.openResourceManager(resourceName);
-               final var pathSummary = revision == -1 ? resMgr.openPathSummary() : resMgr.openPathSummary(revision)) {
+            if (i == Integer.MAX_VALUE) {
+              break; // or (i+1) would overflow
+            }
+          }
 
-            if (!pathSegmentNames.isEmpty()) {
-              final var pathSegmentNameToCheck = pathSegmentNames.removeLast();
-              var pathNodeKeys = initialPathSummaryNodeCheck(pathSummary, pathSegmentNameToCheck);
+          final QNm arrayName = new QNm("__array__");
 
-              while (!pathNodeKeys.isEmpty() && !pathSegmentNames.isEmpty()) {
-                final var finalPathSegmentNameToCheck = pathSegmentNames.removeFirst();
+          for (final var pathNodeKey : pathNodeKeys) {
+            final var currentPathSegmentNames = new ArrayDeque<>(pathSegmentNames);
+            pathSummary.moveTo(pathNodeKey);
+            var candidatePath = pathSummary.getPath();
+            String pathSegment = currentPathSegmentNames.removeFirst();
+            final var pathSteps = candidatePath.steps();
+            boolean found = false;
 
-                final var pathNodeKeysToAdd =
-                    getSubsequentPathNodeKeys(pathSummary, pathNodeKeys, finalPathSegmentNameToCheck);
+            for (int i = pathSteps.size() - 1; i >= 0; i--) {
+              final var step = pathSteps.get(i);
 
-                pathNodeKeys = new ArrayList<>();
-                pathNodeKeys.addAll(pathNodeKeysToAdd);
-              }
+              if (found && currentPathSegmentNames.isEmpty() && !arrayName.equals(step.getValue())) {
+                break;
+              } else if (step.getValue().equals(new QNm(pathSegment))) {
+                found = true;
 
-              if (pathNodeKeys.isEmpty()) {
-                final var parentASTNode = astNode.getParent();
-                parentASTNode.replaceChild(astNode.getChildIndex(), new AST(XQ.SequenceExpr));
-              }
-
-              if (pathSegmentNames.isEmpty()) {
-                boolean notFound = false;
-                final var foundIndexDefs = new ArrayList<>();
-
-                for (final int pathNodeKey : pathNodeKeys) {
-                  final var foundPathNode = pathSummary.getPathNodeForPathNodeKey(pathNodeKey);
-                  final var pathToFoundNode = foundPathNode.getPath(pathSummary);
-
-                  final var indexController = revision == -1
-                      ? resMgr.getRtxIndexController(resMgr.getMostRecentRevisionNumber())
-                      : resMgr.getRtxIndexController(revision);
-
-                  final var indexDef = indexController.getIndexes().findPathIndex(pathToFoundNode);
-
-                  if (indexDef.isEmpty()) {
-                    notFound = true;
-                    break;
-                  }
-
-                  foundIndexDefs.add(indexDef.get());
-                }
-
-                if (!notFound) {
-                  final var parentASTNode = astNode.getParent();
-                  final var indexExpr = new AST(XQExt.IndexExpr);
-                  indexExpr.setProperty("indexDefs", foundIndexDefs);
-                  parentASTNode.replaceChild(astNode.getChildIndex(), indexExpr);
+                if (!currentPathSegmentNames.isEmpty()) {
+                  pathSegment = currentPathSegmentNames.removeFirst();
                 }
               }
             }
+
+            if (!currentPathSegmentNames.isEmpty() || !found) {
+              pathNodeKeysToRemove.add(pathNodeKey);
+            }
+          }
+
+          pathNodeKeys.removeIf(pathNodeKeysToRemove::contains);
+
+          if (pathNodeKeys.isEmpty()) {
+            final var parentASTNode = astNode.getParent();
+            parentASTNode.replaceChild(astNode.getChildIndex(), new AST(XQ.SequenceExpr));
+          }
+
+          boolean notFound = false;
+          final var foundIndexDefs = new ArrayList<>();
+
+          for (final int pathNodeKey : pathNodeKeys) {
+            final var foundPathNode = pathSummary.getPathNodeForPathNodeKey(pathNodeKey);
+            final var pathToFoundNode = foundPathNode.getPath(pathSummary);
+
+            final var indexController = revision == -1
+                ? resMgr.getRtxIndexController(resMgr.getMostRecentRevisionNumber())
+                : resMgr.getRtxIndexController(revision);
+
+            final var indexDef = indexController.getIndexes().findPathIndex(pathToFoundNode);
+
+            if (indexDef.isEmpty()) {
+              notFound = true;
+              break;
+            }
+
+            foundIndexDefs.add(indexDef.get());
+          }
+
+          if (!notFound) {
+            final var parentASTNode = astNode.getParent();
+            final var indexExpr = new AST(XQExt.IndexExpr, XQExt.toName(XQExt.IndexExpr));
+            indexExpr.setProperty("indexDefs", foundIndexDefs);
+            parentASTNode.replaceChild(astNode.getChildIndex(), indexExpr);
+            return indexExpr;
           }
         }
       }
@@ -124,40 +161,14 @@ public final class JsonPathStep extends Walker {
     return astNode;
   }
 
-  private List<Integer> getSubsequentPathNodeKeys(PathSummaryReader pathSummary, ArrayList<Integer> pathNodeKeys,
-      String finalPathSegmentNameToCheck) {
-    final var pathNodeKeysToAdd = new ArrayList<Integer>();
-
-    pathNodeKeys.forEach(pathNodeKey -> {
-      final var descendantPathNodeKeys =
-          pathSummary.matchDescendants(new QNm(finalPathSegmentNameToCheck), pathNodeKey, IncludeSelf.NO);
-
-      for (int i = descendantPathNodeKeys.nextSetBit(0); i >= 0;
-          i = descendantPathNodeKeys.nextSetBit(i + 1)) {
-        // operate on index i here
-        pathNodeKeysToAdd.add(i);
-
-        if (i == Integer.MAX_VALUE) {
-          break; // or (i+1) would overflow
-        }
-      }
-    });
-    return pathNodeKeysToAdd;
-  }
-
-  private ArrayList<Integer> initialPathSummaryNodeCheck(PathSummaryReader pathSummary, String pathSegmentNameToCheck) {
-    final var bitSet = pathSummary.match(new QNm(pathSegmentNameToCheck), 0);
-    var pathNodeKeys = new ArrayList<Integer>();
-
-    for (int i = bitSet.nextSetBit(0); i >= 0; i = bitSet.nextSetBit(i + 1)) {
-      // operate on index i here
-      pathNodeKeys.add(i);
-
-      if (i == Integer.MAX_VALUE) {
-        break; // or (i+1) would overflow
-      }
+  private boolean findDerefAncestor(AST astNode) {
+    boolean foundDerefAncestor = false;
+    AST ancestor = astNode.getParent();
+    while (ancestor != null && !foundDerefAncestor) {
+      foundDerefAncestor = ancestor.getType() == XQ.DerefExpr;
+      ancestor = ancestor.getParent();
     }
-    return pathNodeKeys;
+    return foundDerefAncestor;
   }
 
   private Optional<AST> getPathStep(AST node, Deque<String> pathNames) {
