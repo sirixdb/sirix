c39db90c25681b7f490563eaf32bd64748d747e5
Moshe Uminer <47164590+mosheduminer@users.noreply.github.com>
2020-07-29 16:45:56 -0400

    change permission check code

--- bundles/sirix-rest-api/src/main/kotlin/org/sirix/rest/Auth.kt
+++ bundles/sirix-rest-api/src/main/kotlin/org/sirix/rest/Auth.kt
@@ -8,7 +8,10 @@ import io.vertx.ext.web.RoutingContext
 import io.vertx.kotlin.core.json.json
 import io.vertx.kotlin.core.json.obj
 import io.vertx.kotlin.ext.auth.authentication.authenticateAwait
-import io.vertx.kotlin.ext.auth.isAuthorizedAwait
+import io.vertx.ext.auth.User
+import io.vertx.ext.auth.authorization.PermissionBasedAuthorization
+
+//import io.vertx.kotlin.ext.auth.isAuthorizedAwait
 
 /**
  * Authentication.
@@ -32,10 +35,10 @@ class Auth(private val keycloak: OAuth2Auth, private val role: AuthRole) {
             if (database == null) {
                 false
             } else {
-                user.isAuthorizedAwait(role.databaseRole(database))
+                PermissionBasedAuthorization.create(role.databaseRole(database)).match(user)
             }
 
-        if (!isAuthorized && !user.isAuthorizedAwait(role.keycloakRole())) {
+        if (!isAuthorized && !PermissionBasedAuthorization.create(role.keycloakRole()).match(user)) {
             ctx.fail(HttpResponseStatus.UNAUTHORIZED.code())
             ctx.response().end()
         }
