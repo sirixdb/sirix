da32a2889308d51a0422fafaf92ac9c825bb516f
João Sousa <joao.sousa@feedzai.com>
2021-02-20 16:52:18 +0000

    Add logging and remove redundant suppressions

--- bundles/sirix-core/src/main/java/org/sirix/access/LocalXmlDatabaseFactory.java
+++ bundles/sirix-core/src/main/java/org/sirix/access/LocalXmlDatabaseFactory.java
@@ -3,6 +3,8 @@ package org.sirix.access;
 import org.sirix.access.xml.XmlResourceStore;
 import org.sirix.api.Database;
 import org.sirix.api.xml.XmlResourceManager;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.inject.Inject;
 import javax.inject.Singleton;
@@ -15,12 +17,18 @@ import javax.inject.Singleton;
 @Singleton
 public class LocalXmlDatabaseFactory implements LocalDatabaseFactory<XmlResourceManager> {
 
+    /**
+     * Logger for {@link LocalXmlDatabaseFactory}.
+     */
+    private static final Logger logger = LoggerFactory.getLogger(LocalXmlDatabaseFactory.class);
+
     @Inject
     LocalXmlDatabaseFactory() {}
 
     @Override
     public Database<XmlResourceManager> createDatabase(final DatabaseConfiguration configuration, final User user) {
 
+        logger.trace("Creating new local xml database");
         return new LocalXmlDatabase(configuration, new XmlResourceStore(user));
     }
 }
