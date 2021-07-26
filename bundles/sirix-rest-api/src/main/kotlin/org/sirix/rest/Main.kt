800a2b1405ed8e29ca25bfdb59b68478f469b58f
Johannes Lichtenberger <lichtenberger.johannes@gmail.com>
2021-07-26 23:01:30 +0200

    Update main

--- bundles/sirix-rest-api/src/main/kotlin/org/sirix/rest/Main.kt
+++ bundles/sirix-rest-api/src/main/kotlin/org/sirix/rest/Main.kt
@@ -1,7 +1,10 @@
 package org.sirix.rest
 
 import io.vertx.core.Launcher
+import io.vertx.core.VertxOptions
 
 fun main() {
-    Launcher.executeCommand("run", "-conf", "sirix/bundles/sirix-rest-api/src/main/resources/sirix-conf.json", "org.sirix.rest.SirixVerticle")
+    val launcher = Launcher()
+    launcher.beforeStartingVertx(VertxOptions().setWorkerPoolSize(10).setMaxWorkerExecuteTime(300L * 1000 * 1000000))
+    launcher.execute("run", "-conf", "sirix/bundles/sirix-rest-api/src/main/resources/sirix-conf.json", "org.sirix.rest.SirixVerticle")
 }
