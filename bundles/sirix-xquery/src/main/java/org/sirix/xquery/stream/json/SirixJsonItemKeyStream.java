c4fc0a6c60ed8424ad43e889bea1d95a965ab885
Johannes Lichtenberger <johannes.lichtenberger@sirix.io>
2022-03-05 00:28:32 +0100

    Add test case

--- bundles/sirix-xquery/src/main/java/org/sirix/xquery/stream/json/SirixJsonItemKeyStream.java
+++ bundles/sirix-xquery/src/main/java/org/sirix/xquery/stream/json/SirixJsonItemKeyStream.java
@@ -14,6 +14,7 @@ import org.sirix.node.NodeKind;
 import org.sirix.xquery.json.JsonDBArray;
 import org.sirix.xquery.json.JsonDBCollection;
 import org.sirix.xquery.json.JsonDBObject;
+import org.sirix.xquery.json.JsonItemFactory;
 
 public final class SirixJsonItemKeyStream implements Stream<Item> {
 
@@ -23,13 +24,17 @@ public final class SirixJsonItemKeyStream implements Stream<Item> {
 
   private final JsonNodeReadOnlyTrx rtx;
 
+  private final JsonItemFactory itemFactory;
+
   private Iterator<Long> nodeKeys;
+
 
   public SirixJsonItemKeyStream(final Iterator<NodeReferences> iter, final JsonDBCollection collection,
       final JsonNodeReadOnlyTrx rtx) {
     this.iter = checkNotNull(iter);
     this.collection = checkNotNull(collection);
     this.rtx = checkNotNull(rtx);
+    itemFactory = new JsonItemFactory();
   }
 
   @Override
@@ -51,13 +56,7 @@ public final class SirixJsonItemKeyStream implements Stream<Item> {
     while (nodeKeys.hasNext()) {
       final long nodeKey = nodeKeys.next();
       rtx.moveTo(nodeKey);
-      if (rtx.getKind() == NodeKind.OBJECT) {
-        return new JsonDBObject(rtx, collection);
-      } else if (rtx.getKind() == NodeKind.ARRAY) {
-        return new JsonDBArray(rtx, collection);
-      } else {
-        throw new IllegalStateException("Unexpected node kind: " + rtx.getKind());
-      }
+      return itemFactory.getSequence(rtx, collection);
     }
     return null;
   }
