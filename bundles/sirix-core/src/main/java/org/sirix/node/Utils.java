58ae57cbf779ae3e88a6dea21047eefad1987e08
Johannes Lichtenberger <johannes.lichtenberger@sirix.io>
2022-07-06 23:18:03 +0200

    Work on performance issues with limited "luck" so far

--- bundles/sirix-core/src/main/java/org/sirix/node/Utils.java
+++ bundles/sirix-core/src/main/java/org/sirix/node/Utils.java
@@ -6,7 +6,13 @@ import java.io.IOException;
 import com.google.common.io.ByteArrayDataInput;
 import com.google.common.io.ByteArrayDataOutput;
 
+/**
+ * Copied from Kafka ByteUtils class.
+ */
 public final class Utils {
+
+  private Utils() {
+  }
 
   /**
    * Store a "compressed" variable-length long value.
@@ -14,12 +20,13 @@ public final class Utils {
    * @param output {@link ByteArrayDataOutput} reference
    * @param value long value
    */
-  public static final void putVarLong(final DataOutput output, long value) throws IOException {
-    while ((value & ~0x7F) != 0) {
-      output.write(((byte) ((value & 0x7f) | 0x80)));
-      value >>>= 7;
+  public static void putVarLong(final DataOutput output, long value) throws IOException {
+    long v = (value << 1) ^ (value >> 63);
+    while ((v & 0xffffffffffffff80L) != 0L) {
+      output.writeByte(((int) v & 0x7f) | 0x80);
+      v >>>= 7;
     }
-    output.write((byte) value);
+    output.writeByte((byte) v);
   }
 
   /**
@@ -28,13 +35,22 @@ public final class Utils {
    * @param input {@link ByteArrayDataInput} reference
    * @return long value
    */
-  public static final long getVarLong(final DataInput input) throws IOException {
-    byte singleByte = input.readByte();
-    long value = singleByte & 0x7F;
-    for (int shift = 7; (singleByte & 0x80) != 0; shift += 7) {
-      singleByte = input.readByte();
-      value |= (singleByte & 0x7FL) << shift;
+  public static long getVarLong(final DataInput input) throws IOException {
+    long value = 0L;
+    int i = 0;
+    long b;
+    while (((b = input.readByte()) & 0x80) != 0) {
+      value |= (b & 0x7f) << i;
+      i += 7;
+      if (i > 63)
+        throw illegalVarlongException(value);
     }
-    return value;
+    value |= b << i;
+    return (value >>> 1) ^ -(value & 1);
+  }
+
+  private static IllegalArgumentException illegalVarlongException(long value) {
+    throw new IllegalArgumentException("Varlong is too long, most significant bit in the 10th byte is set, " +
+                                           "converted value: " + Long.toHexString(value));
   }
 }
