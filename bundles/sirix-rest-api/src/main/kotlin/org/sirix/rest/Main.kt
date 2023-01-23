c019370df098f8345c4f54dd23956c5b14d20b92
johannesl <lichtenberger.johannes@gmail.com>
2023-01-23 19:58:58 +0100

    Fix setting of response

--- bundles/sirix-rest-api/src/main/kotlin/org/sirix/rest/Main.kt
+++ bundles/sirix-rest-api/src/main/kotlin/org/sirix/rest/Main.kt
@@ -6,5 +6,5 @@ import io.vertx.core.VertxOptions
 fun main() {
     val launcher = Launcher()
     launcher.beforeStartingVertx(VertxOptions().setWorkerPoolSize(10).setMaxWorkerExecuteTime(300L * 1000 * 1000000))
-    launcher.execute("run", "-conf", "sirix/bundles/sirix-rest-api/src/main/resources/sirix-conf.json", "org.sirix.rest.SirixVerticle")
+    launcher.execute("run", "-conf", "/home/johannes/IdeaProjects/sirix/bundles/sirix-rest-api/src/main/resources/sirix-conf.json", "org.sirix.rest.SirixVerticle")
 }
