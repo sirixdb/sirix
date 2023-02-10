808c6531051c184296b27063006b96ff4e400766
johannesl <lichtenberger.johannes@gmail.com>
2023-02-10 22:55:40 +0100

    Minor cleanup

--- bundles/sirix-core/src/main/java/org/sirix/node/NodeKind.java
+++ bundles/sirix-core/src/main/java/org/sirix/node/NodeKind.java
@@ -1,23 +1,31 @@
 /*
- * Copyright (c) 2011, University of Konstanz, Distributed Systems Group All rights reserved.
- * <p>
- * Redistribution and use in source and binary forms, with or without modification, are permitted
- * provided that the following conditions are met: * Redistributions of source code must retain the
- * above copyright notice, this list of conditions and the following disclaimer. * Redistributions
- * in binary form must reproduce the above copyright notice, this list of conditions and the
- * following disclaimer in the documentation and/or other materials provided with the distribution.
- * * Neither the name of the University of Konstanz nor the names of its contributors may be used to
- * endorse or promote products derived from this software without specific prior written permission.
- * <p>
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR
- * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
- * FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL <COPYRIGHT HOLDER> BE LIABLE
- * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
- * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
- * OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
- * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
- * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ * Copyright (c) 2023, Sirix Contributors
+ *
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *     * Neither the name of the <organization> nor the
+ *       names of its contributors may be used to endorse or promote products
+ *       derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL <COPYRIGHT HOLDER> BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
+
 package org.sirix.node;
 
 import it.unimi.dsi.fastutil.longs.LongArrayList;
@@ -75,6 +83,7 @@ import static org.sirix.node.Utils.putVarLong;
  * @author Sebastian Graf, University of Konstanz
  * @author Johannes Lichtenberger, University of Konstanz
  */
+@SuppressWarnings({ "DuplicatedCode", "unchecked" })
 public enum NodeKind implements DeweyIdSerializer {
 
   /**
@@ -863,7 +872,7 @@ public enum NodeKind implements DeweyIdSerializer {
       var rightSibling = currKey - getVarLong(source);
       var leftSibling = currKey - getVarLong(source);
       var firstChild = currKey - getVarLong(source);
-      var lastChild = firstChild;
+      @SuppressWarnings("UnnecessaryLocalVariable") var lastChild = firstChild;
 
       var config = pageReadTrx.getResourceSession().getResourceConfig();
 
@@ -1476,7 +1485,7 @@ public enum NodeKind implements DeweyIdSerializer {
       } else {
         uncompressedRevisions = revisions;
       }
-      return new RevisionReferencesNode(recordID, revisions);
+      return new RevisionReferencesNode(recordID, uncompressedRevisions);
     }
 
     @Override
@@ -1602,15 +1611,6 @@ public enum NodeKind implements DeweyIdSerializer {
   }
 
   /**
-   * Get class of node.
-   *
-   * @return class of node
-   */
-  public Class<? extends DataRecord> getNodeClass() {
-    return clazz;
-  }
-
-  /**
    * Public method to get the related node based on the identifier.
    *
    * @param id the identifier for the node
@@ -1665,9 +1665,13 @@ public enum NodeKind implements DeweyIdSerializer {
     final long childCount = 0;
     final long descendantCount = 0;
 
-    final StructNodeDelegate structDel =
-        new StructNodeDelegate(nodeDel, firstChild, lastChild, rightSibling, leftSibling, childCount, descendantCount);
-    return structDel;
+    return new StructNodeDelegate(nodeDel,
+                                  firstChild,
+                                  lastChild,
+                                  rightSibling,
+                                  leftSibling,
+                                  childCount,
+                                  descendantCount);
   }
 
   @NotNull
@@ -1699,9 +1703,13 @@ public enum NodeKind implements DeweyIdSerializer {
       descendantCount = getVarLong(source) + childCount;
     }
 
-    final StructNodeDelegate structDel =
-        new StructNodeDelegate(nodeDel, firstChild, lastChild, rightSibling, leftSibling, childCount, descendantCount);
-    return structDel;
+    return new StructNodeDelegate(nodeDel,
+                                  firstChild,
+                                  lastChild,
+                                  rightSibling,
+                                  leftSibling,
+                                  childCount,
+                                  descendantCount);
   }
 
   private static void serializeJsonObjectOrArrayStructDelegate(BytesOut<ByteBuffer> sink, StructNode node,
@@ -1784,8 +1792,6 @@ public enum NodeKind implements DeweyIdSerializer {
         kind == NodeKind.NUMBER_VALUE || kind == NodeKind.STRING_VALUE || kind == NodeKind.BOOLEAN_VALUE
             || kind == NodeKind.NULL_VALUE;
 
-    final var isJsonNode = isValueNode;
-
     final long rightSibling;
     final long leftSibling;
     final long firstChild;
@@ -1817,7 +1823,7 @@ public enum NodeKind implements DeweyIdSerializer {
       descendantCount = getVarLong(source) + childCount;
     }
 
-    if (isJsonNode) {
+    if (isValueNode) {
       return new StructNodeDelegate(nodeDel,
                                     firstChild,
                                     lastChild,
