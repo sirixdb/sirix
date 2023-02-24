f2daa006c31bd5e22dcf4d7ef79294c5d4746301
claudia.madrid <claudiamadrid95@gmail.com>
2023-02-24 16:19:14 -0600

    Change serialize/deserialize for PathPage in PageKind

--- bundles/sirix-core/src/main/java/org/sirix/page/PathPage.java
+++ bundles/sirix-core/src/main/java/org/sirix/page/PathPage.java
@@ -5,7 +5,6 @@ import it.unimi.dsi.fastutil.ints.Int2IntMap;
 import it.unimi.dsi.fastutil.ints.Int2IntOpenHashMap;
 import it.unimi.dsi.fastutil.ints.Int2LongMap;
 import it.unimi.dsi.fastutil.ints.Int2LongOpenHashMap;
-import net.openhft.chronicle.bytes.Bytes;
 import org.checkerframework.checker.nullness.qual.NonNull;
 import org.sirix.access.DatabaseType;
 import org.sirix.api.PageReadOnlyTrx;
@@ -16,7 +15,6 @@ import org.sirix.page.delegates.ReferencesPage4;
 import org.sirix.page.interfaces.Page;
 import org.sirix.settings.Constants;
 
-import java.nio.ByteBuffer;
 
 /**
  * Page to hold references to a value summary.
@@ -54,23 +52,35 @@ public final class PathPage extends AbstractForwardingPage {
     return getOrCreateReference(offset);
   }
 
+  //**
+//   * Read meta page.
+//   *
+//   * @param in input bytes to read from
+//   */
+//  public PathPage(final Bytes<?> in, final SerializationType type) {
+//    delegate = PageUtils.createDelegate(in, type);
+//    final int size = in.readInt();
+//    maxNodeKeys = new Int2LongOpenHashMap((int) Math.ceil(size / 0.75));
+//    for (int i = 0; i < size; i++) {
+//      maxNodeKeys.put(i, in.readLong());
+//    }
+//    final int currentMaxLevelOfIndirectPages = in.readInt();
+//    currentMaxLevelsOfIndirectPages = new Int2IntOpenHashMap((int) Math.ceil(currentMaxLevelOfIndirectPages / 0.75));
+//    for (int i = 0; i < currentMaxLevelOfIndirectPages; i++) {
+//      currentMaxLevelsOfIndirectPages.put(i, in.readByte() & 0xFF);
+//    }
+//  }
   /**
    * Read meta page.
    *
-   * @param in input bytes to read from
+   * @param delegate The references page instance.
+   * @param  maxNodeKeys Maximum node keys.
+   * @param currentMaxLevelsOfIndirectPages Current maximum levels of indirect pages in the tree.
    */
-  PathPage(final Bytes<?> in, final SerializationType type) {
-    delegate = PageUtils.createDelegate(in, type);
-    final int size = in.readInt();
-    maxNodeKeys = new Int2LongOpenHashMap((int) Math.ceil(size / 0.75));
-    for (int i = 0; i < size; i++) {
-      maxNodeKeys.put(i, in.readLong());
-    }
-    final int currentMaxLevelOfIndirectPages = in.readInt();
-    currentMaxLevelsOfIndirectPages = new Int2IntOpenHashMap((int) Math.ceil(currentMaxLevelOfIndirectPages / 0.75));
-    for (int i = 0; i < currentMaxLevelOfIndirectPages; i++) {
-      currentMaxLevelsOfIndirectPages.put(i, in.readByte() & 0xFF);
-    }
+  public PathPage(final Page delegate, final Int2LongMap maxNodeKeys, final Int2IntMap currentMaxLevelsOfIndirectPages){
+    this.delegate = delegate;
+    this.maxNodeKeys = maxNodeKeys;
+    this.currentMaxLevelsOfIndirectPages = currentMaxLevelsOfIndirectPages;
   }
 
   @Override
@@ -112,28 +122,35 @@ public final class PathPage extends AbstractForwardingPage {
     }
   }
 
-  @Override
-  public void serialize(final PageReadOnlyTrx pageReadOnlyTrx, final Bytes<ByteBuffer> out, final SerializationType type) {
-    if (delegate instanceof ReferencesPage4) {
-      out.writeByte((byte) 0);
-    } else if (delegate instanceof BitmapReferencesPage) {
-      out.writeByte((byte) 1);
-    }
-    super.serialize(pageReadOnlyTrx, out, type);
-    final int size = maxNodeKeys.size();
-    out.writeInt(size);
-    for (int i = 0; i < size; i++) {
-      out.writeLong(maxNodeKeys.get(i));
-    }
-    final int currentMaxLevelOfIndirectPages = maxNodeKeys.size();
-    out.writeInt(currentMaxLevelOfIndirectPages);
-    for (int i = 0; i < currentMaxLevelOfIndirectPages; i++) {
-      out.writeByte((byte) currentMaxLevelsOfIndirectPages.get(i));
-    }
-  }
+//  @Override
+//  public void serialize(final PageReadOnlyTrx pageReadOnlyTrx, final Bytes<ByteBuffer> out, final SerializationType type) {
+//    if (delegate instanceof ReferencesPage4) {
+//      out.writeByte((byte) 0);
+//    } else if (delegate instanceof BitmapReferencesPage) {
+//      out.writeByte((byte) 1);
+//    }
+//    super.serialize(pageReadOnlyTrx, out, type);
+//    final int size = maxNodeKeys.size();
+//    out.writeInt(size);
+//    for (int i = 0; i < size; i++) {
+//      out.writeLong(maxNodeKeys.get(i));
+//    }
+//    final int currentMaxLevelOfIndirectPages = maxNodeKeys.size();
+//    out.writeInt(currentMaxLevelOfIndirectPages);
+//    for (int i = 0; i < currentMaxLevelOfIndirectPages; i++) {
+//      out.writeByte((byte) currentMaxLevelsOfIndirectPages.get(i));
+//    }
+//  }
 
   public int getCurrentMaxLevelOfIndirectPages(int index) {
     return currentMaxLevelsOfIndirectPages.get(index);
+  }
+  /**
+   * Get the size of CurrentMaxLevelOfIndirectPages to Serialize
+   * @return int Size of CurrentMaxLevelOfIndirectPages
+   */
+  public int getCurrentMaxLevelOfIndirectPagesSize(){
+    return currentMaxLevelsOfIndirectPages.size();
   }
 
   public int incrementAndGetCurrentMaxLevelOfIndirectPages(int index) {
@@ -149,6 +166,14 @@ public final class PathPage extends AbstractForwardingPage {
    */
   public long getMaxNodeKey(final int indexNo) {
     return maxNodeKeys.get(indexNo);
+  }
+
+  /**
+   * Get the size of MaxNodeKey to Serialize
+   * @return int Size of MaxNodeKey
+   */
+  public int getMaxNodeKeySize(){
+    return maxNodeKeys.size();
   }
 
   public long incrementAndGetMaxNodeKey(final int indexNo) {
