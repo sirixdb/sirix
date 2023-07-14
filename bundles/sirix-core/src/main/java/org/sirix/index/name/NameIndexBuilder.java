d0282fdb7f85b8abecf8c89eb38be5c86959fb0a
Johannes Lichtenberger <johannes.lichtenberger@sirix.io>
2023-07-14 17:27:09 +0200

    Minor refactorings

--- bundles/sirix-core/src/main/java/org/sirix/index/name/NameIndexBuilder.java
+++ bundles/sirix-core/src/main/java/org/sirix/index/name/NameIndexBuilder.java
@@ -17,26 +17,26 @@ import java.util.Set;
 public final class NameIndexBuilder {
   private static final LogWrapper LOGGER = new LogWrapper(LoggerFactory.getLogger(NameIndexBuilder.class));
 
-  public Set<QNm> mIncludes;
-  public Set<QNm> mExcludes;
-  public RBTreeWriter<QNm, NodeReferences> mAVLTreeWriter;
+  public Set<QNm> includes;
+  public Set<QNm> excludes;
+  public RBTreeWriter<QNm, NodeReferences> indexWriter;
 
   public NameIndexBuilder(final Set<QNm> includes, final Set<QNm> excludes,
-      final RBTreeWriter<QNm, NodeReferences> avlTreeWriter) {
-    mIncludes = includes;
-    mExcludes = excludes;
-    mAVLTreeWriter = avlTreeWriter;
+      final RBTreeWriter<QNm, NodeReferences> indexWriter) {
+    this.includes = includes;
+    this.excludes = excludes;
+    this.indexWriter = indexWriter;
   }
 
   public VisitResultType build(QNm name, ImmutableNode node) {
-    final boolean included = (mIncludes.isEmpty() || mIncludes.contains(name));
-    final boolean excluded = (!mExcludes.isEmpty() && mExcludes.contains(name));
+    final boolean included = (includes.isEmpty() || includes.contains(name));
+    final boolean excluded = (!excludes.isEmpty() && excludes.contains(name));
 
     if (!included || excluded) {
       return VisitResultType.CONTINUE;
     }
 
-    final Optional<NodeReferences> textReferences = mAVLTreeWriter.get(name, SearchMode.EQUAL);
+    final Optional<NodeReferences> textReferences = indexWriter.get(name, SearchMode.EQUAL);
 
     try {
       textReferences.ifPresentOrElse(nodeReferences -> setNodeReferences(node, nodeReferences, name),
@@ -49,6 +49,6 @@ public final class NameIndexBuilder {
   }
 
   private void setNodeReferences(final ImmutableNode node, final NodeReferences references, final QNm name) {
-    mAVLTreeWriter.index(name, references.addNodeKey(node.getNodeKey()), MoveCursor.NO_MOVE);
+    indexWriter.index(name, references.addNodeKey(node.getNodeKey()), MoveCursor.NO_MOVE);
   }
 }
