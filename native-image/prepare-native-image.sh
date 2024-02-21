014d56a157f1e8cc5dcb39d9836e7bc7396eb5fb
johannesl <lichtenberger.johannes@gmail.com>
2024-02-22 00:15:27 +0100

    Update native-image generation

--- native-image/prepare-native-image.sh
+++ native-image/prepare-native-image.sh
@@ -18,9 +18,7 @@
 source "$HOME/.sdkman/bin/sdkman-init.sh"
 sdk use java 21.0.2-graal 1>&2
 
-# ./mvnw clean verify removes target/ and will re-trigger native image creation.
-if [ ! -f sirix-query-0.10.6-all ]; then
-
+if [ ! -f sirix-shell ]; then
     # Performance tuning flags, optimization level 3, maximum inlining exploration, and compile for the architecture where the native image is generated.
     NATIVE_IMAGE_OPTS="-O3 -H:TuneInlinerExploration=1 -march=native --gc=G1 --strict-image-heap --pgo-instrument --no-fallback"
    
@@ -28,5 +26,5 @@ if [ ! -f sirix-query-0.10.6-all ]; then
     
     NATIVE_IMAGE_OPTS="$NATIVE_IMAGE_OPTS -DLOGGER_HOME=~/sirix-data --add-exports=java.base/jdk.internal.ref=ALL-UNNAMED --add-exports=java.base/sun.nio.ch=ALL-UNNAMED --add-exports=jdk.unsupported/sun.misc=ALL-UNNAMED --add-exports=jdk.compiler/com.sun.tools.javac.file=ALL-UNNAMED --add-opens=jdk.compiler/com.sun.tools.javac=ALL-UNNAMED --add-opens=java.base/java.lang=ALL-UNNAMED --add-opens=java.base/java.lang.reflect=ALL-UNNAMED --add-opens=java.base/java.io=ALL-UNNAMED --add-opens=java.base/java.util=ALL-UNNAMED"
     
-    native-image $NATIVE_IMAGE_OPTS -jar ../bundles/sirix-query/build/libs/sirix-query-0.10.6-all.jar sirix-shell
+    native-image $NATIVE_IMAGE_OPTS -cp ../bundles/sirix-query/build/libs/sirix-query-0.11.0-all.jar:. -o sirix-shell io.sirix.query.Main
 fi
