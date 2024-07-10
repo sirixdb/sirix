bbc6e95aa2c2ac37cfad0fb01d23dbd3863bcd03
johannesl <lichtenberger.johannes@gmail.com>
2024-07-10 18:35:50 +0200

    Fix configs due to new Keycloak version

--- bundles/sirix-rest-api/src/test/kotlin/io/sirix/rest/SirixVerticleJsonTest.kt
+++ bundles/sirix-rest-api/src/test/kotlin/io/sirix/rest/SirixVerticleJsonTest.kt
@@ -43,7 +43,7 @@ class SirixVerticleJsonTest {
         val options = DeploymentOptions().setConfig(
             JsonObject().put("port", 9443)
                 .put("client.secret", "78a294c4-0492-4e44-a35f-7eb9cab0d831") // "64aaf9b2-9ea1-43cd-bcb6-87d2f430aaa2"
-                .put("keycloak.url", "http://localhost:8080/auth/realms/sirixdb")
+                .put("keycloak.url", "http://localhost:8080/realms/sirixdb")
         )
         vertx.deployVerticle("io.sirix.rest.SirixVerticle", options, testContext.succeedingThenComplete())
 
