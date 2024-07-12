2535718735d010c84c4d7ea5e6cb22182cb85499
johannesl <lichtenberger.johannes@gmail.com>
2024-07-12 23:34:13 +0200

    Update to use keycloak/localhost

--- bundles/sirix-rest-api/src/test/resources/wait-for-keycloak.sh
+++ bundles/sirix-rest-api/src/test/resources/wait-for-keycloak.sh
@@ -3,13 +3,18 @@
 # Start Keycloak in the background
 /opt/keycloak/bin/kc.sh "$@" &
 
+# Determine if running in GitHub Actions or locally
+if [ "$GITHUB_ACTIONS" == "true" ]; then
+  KEYCLOAK_HOST="keycloak"
+else
+  KEYCLOAK_HOST="localhost"
+fi
+
 # Wait for Keycloak to be ready
-until curl -sf http://localhost:8080 > /dev/null; do
+until curl -sf http://$KEYCLOAK_HOST:8080 > /dev/null; do
   echo "Waiting for Keycloak to be ready..."
   sleep 5
 done
-
-sleep 20
 
 # Keycloak is ready, run the create-sirix-users.sh script
 /opt/keycloak/scripts/create-sirix-users.sh
