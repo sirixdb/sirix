cc8e55d9cbcb0abe35c1dc065c902123ecef7bab
johannesl <lichtenberger.johannes@gmail.com>
2024-07-13 19:20:29 +0200

    Minor changes

--- bundles/sirix-rest-api/src/test/resources/wait-for-keycloak.sh
+++ bundles/sirix-rest-api/src/test/resources/wait-for-keycloak.sh
@@ -3,15 +3,8 @@
 # Start Keycloak in the background
 /opt/keycloak/bin/kc.sh "$@" &
 
-# Determine if running in GitHub Actions or locally
-if [ "$GITHUB_ACTIONS" == "true" ]; then
-  KEYCLOAK_HOST="keycloak"
-else
-  KEYCLOAK_HOST="localhost"
-fi
-
 # Wait for Keycloak to be ready
-until curl -sf http://$KEYCLOAK_HOST:8080 > /dev/null; do
+until curl -sf http://keycloak:8080 > /dev/null; do
   echo "Waiting for Keycloak to be ready..."
   sleep 5
 done
