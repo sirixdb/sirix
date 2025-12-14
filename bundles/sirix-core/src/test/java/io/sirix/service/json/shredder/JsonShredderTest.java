25d235c46aaf250a8f982106a915ea1d84028120
Johannes Lichtenberger <johannes.lichtenberger@sirix.io>
2025-12-14 19:01:06 +0100

    Disable test again

--- bundles/sirix-core/src/test/java/io/sirix/service/json/shredder/JsonShredderTest.java
+++ bundles/sirix-core/src/test/java/io/sirix/service/json/shredder/JsonShredderTest.java
@@ -210,7 +210,7 @@ public final class JsonShredderTest {
   // Use Shenandoah or ZGC
   // JVM flags: -XX:+UseShenandoahGC -Xlog:gc -XX:+UnlockExperimentalVMOptions -XX:+AlwaysPreTouch -XX:+UseLargePages -XX:+DisableExplicitGC -XX:+PrintCompilation -XX:ReservedCodeCacheSize=1000m -XX:+UnlockDiagnosticVMOptions -XX:+PrintInlining -XX:EliminateAllocationArraySizeLimit=1024
   @Test
-  //@Disabled
+  @Disabled
   public void testShredderAndTraverseChicago() {
     logger.info("start");
     final var jsonPath = JSON.resolve("cityofchicago.json");
